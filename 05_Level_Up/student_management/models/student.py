984a613511e648453cb52a2bb1b222ed91d4bc15
Nguyen Ho Vu <vunhpc06500@fpt.edu.vn>
2026-04-29 22:53:25 +0700

    Refactors student model with enhanced validation

--- 05_Level_Up/student_management/models/student.py
+++ 05_Level_Up/student_management/models/student.py
@@ -1,3 +1,6 @@
+import re
+
+
 class Student:
     def __init__(self, student_id: str, full_name: str, age: int, gpa: float):
         self.student_id = student_id
@@ -11,11 +14,8 @@ class Student:
 
     @student_id.setter
     def student_id(self, value):
-        if not Student.is_valid_student_id(value):
-            raise ValueError(
-                "Mã sinh viên phải bắt đầu bằng ID và không được bỏ trống!"
-            )
-
+        if not Student.validate_code(value):
+            raise ValueError("Mã sinh viên không đúng định dạng")
         self._student_id = value.strip().upper()
 
     @property
@@ -25,7 +25,7 @@ class Student:
     @full_name.setter
     def full_name(self, value):
         if not isinstance(value, str) or not value.strip():
-            raise ValueError("Tên sinh viên không được bỏ trống!")
+            raise ValueError("Tên sinh viên không được rỗng!")
         self._full_name = value.strip().title()
 
     @property
@@ -35,7 +35,7 @@ class Student:
     @age.setter
     def age(self, value):
         if not isinstance(value, int) or value < 16:
-            raise ValueError("Tuổi sinh viên phải từ 16 trở lên")
+            raise ValueError("Tuổi phải >= 16")
         self._age = value
 
     @property
@@ -45,8 +45,14 @@ class Student:
     @gpa.setter
     def gpa(self, value):
         if not isinstance(value, (float, int)) or not (0 <= value <= 10):
-            raise ValueError("Điểm trung bình phải từ 0 đến 10")
+            raise ValueError("Điểm phải từ 0 đến 10!")
         self._gpa = float(value)
+
+    @staticmethod
+    def validate_code(student_id):
+        if not isinstance(student_id, str):
+            return False
+        return bool(re.match(r"^ID_\d{3}$", student_id.strip().upper()))
 
     @property
     def rating(self):
@@ -60,14 +66,13 @@ class Student:
             return "Yếu"
 
     def __str__(self):
-        return f"{self.student_id} | {self.full_name} | {self.age} | {self.gpa} | {self.rating}"
+        return f"{self.student_id} | {self.full_name} | {self.age} | {self.gpa:.2f} | {self.rating}"
 
-    @staticmethod
-    def is_valid_student_id(student_ids):
-        return isinstance(student_ids, str) and student_ids.strip().upper().startswith(
-            "ID"
-        )
-
-    @classmethod
-    def create_default(cls):
-        return cls("ID_001", "Nguyễn Hồ Vũ", 22, 6)
+    # Convert Object -> Dictionary(Json)
+    def to_dict(self):
+        return {
+            "student_id": self.student_id,
+            "full_name": self.full_name,
+            "age": self.age,
+            "gpa": self.gpa,
+        }
